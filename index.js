92f585550815a6f977f45b894ef8bcea591e8408
nacat <nanahow0911@gmail.com>
2015-10-12 23:23:06 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function change(obj)
-{var a=obj.height; obj.height=a*1.25;var b=obj.width;obj.width=b*1.25;}
+{var a=obj.height;obj.height=a*1.25;var b=obj.width;obj.width=b*1.25;}
 function recover(obj)
-{var c=obj.height; obj.height=c*0.8;var d=obj.width;obj.width=d*0.8;}
+{var c=obj.height;obj.height=c*0.8;var d=obj.width;obj.width=d*0.8;}
 function big(obj)
 {if(obj.innerHTML=="Thank You")obj.innerHTML="Nana's Resume";else obj.innerHTML="Thank You";}
